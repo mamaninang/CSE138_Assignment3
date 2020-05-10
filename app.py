0b12c378ff1516b9a536f04fd49f711dc6b1b963
Mariah Maninang <mmaninan@ucsc.edu>
2020-05-09 20:04:48 -0700

    working on checking for causal-metadata

--- app.py
+++ app.py
@@ -9,11 +9,13 @@
 from flask import Flask, request, make_response, jsonify
 import os
 import requests
+import json
 app = Flask(__name__)
 
 key_value_store = {}
-socketAddress = os.getenviron("SOCKET_ADDRESS")     
+ip, port = os.getenv('SOCKET_ADDRESS').split(':')[0], os.getenv('SOCKET_ADDRESS').split(':')[1]
 vectorClock = {"10.10.0.2": 0, "10.10.0.3":0, "10.10.0.4":0}
+requestQueue = {}
 
 @app.route('/key-value-store/<key>', methods=['GET','PUT','DELETE'])
 def kvs(key):
@@ -22,28 +24,38 @@ def kvs(key):
     #vectorClockJson = json.loads(vectorClock)
 
     if request.method == 'GET':
-        return make_response({"doesExist":true,"message":"Retrieved successfully","value":"%s"} % key_value_store[key], 200) \
+        return make_response('{"doesExist":true,"message":"Retrieved successfully","value":"%s"}' % key_value_store[key], 200) \
             if key in key_value_store else make_response('{"doesExist":false,"error":"Key does not exist","message":"Error in GET"}', 404)
 
     if request.method == 'PUT':
-        causalMetadata = request.json["causal-metadata"]
+        causalMetadata = json.loads(json.dumps(request.json["causal-metadata"]))
 
+        #if no causal metadata, then complete request right away
+        if causalMetadata == "":
+            key_value_store[key] = request.json
+            vectorClock[ip] += 1
+            vectorClockJson = json.dumps(vectorClock)
+            checkRequestQueue(key, causalMetadata) 
 
-        if causalMetadata is None:
-            kvs[key] = request.json
-            vectorClock[socketAddress] += 1
-            checkRequestQueue(key, causalMetadata)
-            return make_response(jsonify('{"message":"Added successfully", "causal-metadata": "{vectorClock}"}'), 201)
+            return make_response('{"message":"Added successfully", "causal-metadata": "%s"}' % vectorClockJson, 201)
+
+        #if there is metadata, do the following
         else:
 
-            #checks if request in requestQueue has same causal metadata
-            if causalMetadata is requestQueue[key]["causal-metadata"]:     #same as checkRequestQueue??
-                kvs[key] = requestQueue[key]
-                del requestQueue[key]
-                vectorClock[socketAddress] += 1
-                return make_response(jsonify('{"message":"Added successfully", "causal-metadata": "{vectorClock}"}'), 201)
+            #checks if current vector clock is equal to metadata
+            if vectorClock == causalMetadata:     
+                key_value_store[key] = request.json
+                vectorClock[ip] += 1
+                vectorClockJson = json.dumps(vectorClock)
+                #if requestQueue != {}:
+                    #checkRequestQueue(key, causalMetadata) 
+                
+                vectorType = type(vectorClockJson)
+                return make_response('{"message":"Added successfully", "causal-metadata": "%s"}' % vectorClockJson, 201)
             else:
                 requestQueue[key] = request.json
+                return make_response('{"message": "The request has been placed in a queue. Waiting for another write"}', 201)
+
 
         #after completing put request, check if any request in requestQueue causally depend on current request
         #checkRequestQueue(key, causalMetadata)
@@ -59,11 +71,13 @@ def kvs(key):
             return make_response('{"doesExist":false,"error":"Key does not exist","message":"Error in DELETE"}', 404)
     
 def checkRequestQueue(key, causalMetadata):
-    if key in requestQueue and causalMetadata in requestQueue[key]:
+
+    #key in requestQueue and causal-metadata is same as current clock, store request in kvs
+    if key in requestQueue and (requestQueue[key]["causal-metadata"] == vectorClock):
         kvs[key] = requestQueue[key]
         del requestQueue[key]
         vectorClock[socketAddress] += 1
-        return make_response(jsonify('{"message":"Added successfully", "causal-metadata": "{vectorClock}"}'), 201)
+        return make_response(jsonify('{"message":"Added successfully", "causal-metadata": "%s"}' %vectorClockJson), 201)
     #elif key in kvs   
 
 if __name__ == '__main__':
