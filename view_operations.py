63cee8c22bd3f8d665dda9a78aa1bd5aecf9f687
Omar Quinones <oquinone@ucsc.edu>
2020-05-12 19:04:17 -0700

    Working on updating replicas view

--- view_operations.py
+++ view_operations.py
@@ -10,16 +10,24 @@ request_address = os.getenv('SOCKET_ADDRESS')
 
 @app.route('/key-value-store-view/<key>', methods=['GET', 'PUT', 'DELETE'])
 def view_operations(key):
-    address = 'http://' + ip_address + request.path
+    #address = 'http://' + ip_address + request.path
     if request.method == 'GET':
         return {"message":"View retrieved succesfully","view":view}, 200
 
-    elif requests.method == 'PUT':
-        return hi;
+    elif request.method == 'PUT':
+        address_to_be_added = os.getenv('socket-address')
+        if address_to_be_added in view_list:
+            return {"error":"Socket address already exists in the view", "message":"Error in PUT"}, 404
+        else:
+            view + ',' + address_to_be_added
+            
     
-    elif requests.method == 'DELETE':
+    elif request.method == 'DELETE':
         address_to_delete = os.getenv('socket-address')
-        if address_to_delete in view_list:
+        #if address_to_delete in view_list:
+
+# def broadcast_Put():
+
 
 if __name__=='__main__':
-    app.run(debug=True, host-'0.0.0.0', port=8085)
+    app.run(debug=True, host='0.0.0.0', port=8085)
