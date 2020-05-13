2d94b2f14f7fdc8a772d9b1a7d0759220fd5e717
Omar Quinones <oquinone@ucsc.edu>
2020-05-12 21:08:47 -0700

    Updating get

--- view_operations.py
+++ view_operations.py
@@ -3,31 +3,36 @@ import requests
 import os
 app = Flask(__name__)
 
-key_value_store = {}
 view = os.getenv('VIEW')
 view_list = view.split(",");
 request_address = os.getenv('SOCKET_ADDRESS')
 
-@app.route('/key-value-store-view/<key>', methods=['GET', 'PUT', 'DELETE'])
-def view_operations(key):
-    #address = 'http://' + ip_address + request.path
-    if request.method == 'GET':
-        return {"message":"View retrieved succesfully","view":view}, 200
-
-    elif request.method == 'PUT':
-        address_to_be_added = os.getenv('socket-address')
-        if address_to_be_added in view_list:
-            return {"error":"Socket address already exists in the view", "message":"Error in PUT"}, 404
-        else:
-            view + ',' + address_to_be_added
-            
+@app.route('/key-value-store-view', methods=['GET', 'PUT', 'DELETE'])
+def view_operations():
+    if request_address is None:
     
-    elif request.method == 'DELETE':
-        address_to_delete = os.getenv('socket-address')
-        #if address_to_delete in view_list:
+        if request.method == 'GET':
+            return {"message":"View retrieved succesfully","view":view}, 200
 
-# def broadcast_Put():
+        elif request.method == 'PUT':
+            address_to_be_added = request.json["socket-address"]
+            if address_to_be_added in view_list:
+                return {"error":"Socket address already exists in the view", "message":"Error in PUT"}, 404
+            else:
+                view + ',' + address_to_be_added
+                
+        
+        elif request.method == 'DELETE':
+            address_to_delete = os.getenv('socket-address')
+            #if address_to_delete in view_list:
+    
+    else:
+        response_address = 'http://' + request_address + "/key-value-store-view"
+        headers = {"Content-Type": "application/json"}
 
+        if request.method == 'GET':
+            return requests.get(response_address, headers=headers)
+    
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=8085)
