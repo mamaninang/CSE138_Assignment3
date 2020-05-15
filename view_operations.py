0db91882e72d5e1f2fac9a4843a262b9c546c689
Omar Quinones <oquinone@ucsc.edu>
2020-05-14 19:44:36 -0700

    Everything works locally, need to run it against script

--- view_operations.py
+++ view_operations.py
@@ -16,6 +16,19 @@ def broadcast_put():
         
         return {"message":"Replica added successfully to the view"}, 201
 
+@app.route('/key-value-store-view/del', methods=['DELETE'])
+def brodcast_del():
+    if request.method == 'DELETE':
+        replica_view = os.getenv('VIEW')
+        addres_to_be_deleted = request.args['socket-address']
+        replica_view_list = replica_view.split(',')
+        replica_view_list.remove(addres_to_be_deleted)
+        hold_new_view = ",".join(replica_view_list)
+        os.environ['VIEW'] = hold_new_view
+
+        return {"message":"Replica deleted successfully from the view"}, 200
+
+
 @app.route('/key-value-store-view', methods=['GET', 'PUT', 'DELETE'])
 def view_operations():
     replica_view = os.getenv('VIEW')
@@ -38,32 +51,42 @@ def view_operations():
 
             for ip in view_list:
                 if ip != socket_address:
-                    address_to_search = 'http://' + ip + '/key-value-store-view'
-                    response = requests.get(address_to_search, headers=headers)
-                    response_json = response.json()
-                    response_view = response_json['view']
+                    response_view, address = get_view(ip)
+
                     if response_view != replica_view:
-                        address_to_search = address_to_search + "/put?socket-address=" + address_to_be_added
+                        address_to_search = address + "/put?socket-address=" + address_to_be_added
                         requests.put(address_to_search, headers=headers, json=request.json)
 
             return {"message":"Replica added successfully to the view"}, 201
             
-    
     elif request.method == 'DELETE':
         address_to_delete = request.args['socket-address']
+
         if address_to_delete not in view_list:
             return {"error":"Socket address does not exist in the view","message":"Error in DELETE"}, 404
+
         else:
             view_list.remove(address_to_delete)
-            new_env = ",".join(view_list)
-            os.environ['VIEW'] = new_env
+            updated_view = ",".join(view_list)
+            os.environ['VIEW'] = updated_view
+            replica_view = os.getenv('VIEW')
 
-            # for ip in view:
-            #     address_to_search = "http://" + ip + 'key-value-store-view'
-            #     checking_ips_view = request.get(address_to_delete, headers=headers)
-            #     if address_to_delete in checking_ips_view.view:
-            #         request.put(address_to_search, headers=headers, data={'socket-address': address_to_delete})
+            for ip in view_list:
+                if ip != socket_address and ip != address_to_delete:
+                    response_view, address = get_view(ip)
+
+                    if response_view != replica_view:
+                        address_to_search = address + "/del?socket-address=" + address_to_delete
+                        requests.delete(address_to_search, headers=headers, json=request.json)
+
             return {"message":"Replica deleted successfully from the view"}, 200
+
+def get_view(ip_address):
+    address_to_search = 'http://' + ip_address + '/key-value-store-view'
+    response = requests.get(address_to_search, headers=headers)
+    response_json = response.json()
+    response_view = response_json['view']
+    return response_view, address_to_search
 
 
 if __name__=='__main__':
