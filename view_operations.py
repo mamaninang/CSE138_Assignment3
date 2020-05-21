17c1bedad461bd204f42ccd9faf78d793a5c5767
omar quinones <omarquinones@omars-mbp.attlocal.net>
2020-05-21 15:19:34 -0700

    Removed duplicate code and made broadcast function

--- view_operations.py
+++ view_operations.py
@@ -22,23 +22,19 @@ app = Flask(__name__)
 
 headers = {"Content-Type": "application/json"}
 
-#Function to add a replica to the view of the given replica
-@app.route('/key-value-store-view/put', methods=['PUT'])
+#Updates replicas VIEW with either DELETE or PUT operation
+@app.route('/key-value-store-view/broadcast', methods=['PUT', 'DELETE'])
 def broadcast_put():
     if request.method == 'PUT':
         replica_view = os.getenv('VIEW')
-        address_to_be_added = request.args['socket-address']
+        address_to_be_added = request.json['socket-address']
         updated_view = replica_view + "," + address_to_be_added
         os.environ['VIEW'] = updated_view
         
         return {"message":"Replica added successfully to the view"}, 201
-
-#Function to delete a replica from the view of the given replica
-@app.route('/key-value-store-view/del', methods=['DELETE'])
-def brodcast_del():
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE':
         replica_view = os.getenv('VIEW')
-        addres_to_be_deleted = request.args['socket-address']
+        addres_to_be_deleted = request.json['socket-address']
         replica_view_list = replica_view.split(',')
         replica_view_list.remove(addres_to_be_deleted)
         hold_new_view = ",".join(replica_view_list)
@@ -58,55 +54,55 @@ def view_operations():
         return {"message":"View retrieved succesfully","view":replica_view}, 200
 
     elif request.method == 'PUT':
-        address_to_be_added = request.args['socket-address']
+        address_to_be_added = request.json['socket-address']
 
         if address_to_be_added in view_list:
             return {"error":"Socket address already exists in the view","message":"Error in PUT"}, 404
 
         else:
+            #updating the given replicas view
             updated_view = replica_view + "," + address_to_be_added
             os.environ['VIEW'] = updated_view
             replica_view = os.getenv('VIEW')
 
-            for ip in view_list:
-                if ip != socket_address:
-                    response_view, address = get_view(ip)
-
-                    if response_view != replica_view:
-                        address_to_search = address + "/put?socket-address=" + address_to_be_added
-                        requests.put(address_to_search, headers=headers, json=request.json)
-
+            #Calling broadcasting function on a PUT operation
+            sending_broadcast_message(view_list,socket_address, address_to_be_added, replica_view)
             return {"message":"Replica added successfully to the view"}, 201
             
     elif request.method == 'DELETE':
-        address_to_delete = request.args['socket-address']
+        address_to_delete = request.json['socket-address']
 
         if address_to_delete not in view_list:
             return {"error":"Socket address does not exist in the view","message":"Error in DELETE"}, 404
 
         else:
+            #updating given replicas view
             view_list.remove(address_to_delete)
             updated_view = ",".join(view_list)
             os.environ['VIEW'] = updated_view
             replica_view = os.getenv('VIEW')
 
-            for ip in view_list:
-                if ip != socket_address and ip != address_to_delete:
-                    response_view, address = get_view(ip)
-
-                    if response_view != replica_view:
-                        address_to_search = address + "/del?socket-address=" + address_to_delete
-                        requests.delete(address_to_search, headers=headers, json=request.json)
-
+            #Calling broadcasting function 
+            sending_broadcast_message(view_list,socket_address, address_to_delete, replica_view)
             return {"message":"Replica deleted successfully from the view"}, 200
 
-#Function to get other replicas view and returns the new address to brodcast messages
+#returns replicas view and url
 def get_view(ip_address):
     address_to_search = 'http://' + ip_address + '/key-value-store-view'
     response = requests.get(address_to_search, headers=headers)
     response_json = response.json()
     response_view = response_json['view']
     return response_view, address_to_search
+
+#Loop to broadcast a message 
+def sending_broadcast_message(view_list,socket_address, address_to_delete, replica_view):
+    for ip in view_list:
+        if ip != socket_address and ip != address_to_delete:
+            response_view, address = get_view(ip)
+
+            if response_view != replica_view:
+                address_to_search = address + "/broadcast?socket-address=" + address_to_delete
+                requests.delete(address_to_search, headers=headers, json=request.json)
 
 
 if __name__=='__main__':
